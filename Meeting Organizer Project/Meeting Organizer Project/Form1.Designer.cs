7757ceddf8e333efb192270a9d60c8c1668d2272
FatihOzkaya18 <34345364+FatihOzkaya18@users.noreply.github.com>
2017-12-07 18:45:05 +0300

    master

---
+++ Meeting Organizer Project/Meeting Organizer Project/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Meeting_Organizer_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.txtKatilimcilar = new System.Windows.Forms.TextBox();
+            this.dateTarih = new System.Windows.Forms.DateTimePicker();
+            this.txtKonu = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBasSaati = new System.Windows.Forms.MaskedTextBox();
+            this.txtBitSaati = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(318, 10);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(639, 185);
+            this.dataGridView1.TabIndex = 35;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Image = ((System.Drawing.Image)(resources.GetObject("btnTemizle.Image")));
+            this.btnTemizle.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnTemizle.Location = new System.Drawing.Point(833, 216);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(124, 52);
+            this.btnTemizle.TabIndex = 33;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Image = ((System.Drawing.Image)(resources.GetObject("btnGuncelle.Image")));
+            this.btnGuncelle.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnGuncelle.Location = new System.Drawing.Point(497, 216);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(124, 52);
+            this.btnGuncelle.TabIndex = 32;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Image = ((System.Drawing.Image)(resources.GetObject("btnEkle.Image")));
+            this.btnEkle.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnEkle.Location = new System.Drawing.Point(318, 216);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(124, 52);
+            this.btnEkle.TabIndex = 30;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Image = ((System.Drawing.Image)(resources.GetObject("btnSil.Image")));
+            this.btnSil.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSil.Location = new System.Drawing.Point(665, 216);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(124, 52);
+            this.btnSil.TabIndex = 31;
+            this.btnSil.Text = "Sil";
+            this.btnSil.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // txtKatilimcilar
+            // 
+            this.txtKatilimcilar.Location = new System.Drawing.Point(137, 243);
+            this.txtKatilimcilar.Name = "txtKatilimcilar";
+            this.txtKatilimcilar.Size = new System.Drawing.Size(164, 20);
+            this.txtKatilimcilar.TabIndex = 29;
+            // 
+            // dateTarih
+            // 
+            this.dateTarih.Location = new System.Drawing.Point(137, 81);
+            this.dateTarih.Name = "dateTarih";
+            this.dateTarih.Size = new System.Drawing.Size(164, 20);
+            this.dateTarih.TabIndex = 26;
+            // 
+            // txtKonu
+            // 
+            this.txtKonu.Location = new System.Drawing.Point(137, 10);
+            this.txtKonu.Multiline = true;
+            this.txtKonu.Name = "txtKonu";
+            this.txtKonu.Size = new System.Drawing.Size(164, 44);
+            this.txtKonu.TabIndex = 25;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(12, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 18);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Katılımcılar:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(12, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 18);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Bitiş Saati:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 18);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Başlangıç Saati:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 18);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Tarih:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 18);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Toplantı Konusu:";
+            // 
+            // txtBasSaati
+            // 
+            this.txtBasSaati.Location = new System.Drawing.Point(137, 134);
+            this.txtBasSaati.Mask = "90:00";
+            this.txtBasSaati.Name = "txtBasSaati";
+            this.txtBasSaati.Size = new System.Drawing.Size(164, 20);
+            this.txtBasSaati.TabIndex = 36;
+            this.txtBasSaati.ValidatingType = typeof(System.DateTime);
+            // 
+            // txtBitSaati
+            // 
+            this.txtBitSaati.Location = new System.Drawing.Point(137, 190);
+            this.txtBitSaati.Mask = "90:00";
+            this.txtBitSaati.Name = "txtBitSaati";
+            this.txtBitSaati.Size = new System.Drawing.Size(164, 20);
+            this.txtBitSaati.TabIndex = 37;
+            this.txtBitSaati.ValidatingType = typeof(System.DateTime);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(981, 285);
+            this.Controls.Add(this.txtBitSaati);
+            this.Controls.Add(this.txtBasSaati);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.txtKatilimcilar);
+            this.Controls.Add(this.dateTarih);
+            this.Controls.Add(this.txtKonu);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Meeting Organizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.TextBox txtKatilimcilar;
+        private System.Windows.Forms.DateTimePicker dateTarih;
+        private System.Windows.Forms.TextBox txtKonu;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox txtBasSaati;
+        private System.Windows.Forms.MaskedTextBox txtBitSaati;
+    }
+}
+
---
+++ Meeting Organizer Project/Meeting Organizer Project/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Meeting_Organizer_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GridDoldur();
+        }
+
+        private void GridDoldur()//DataGrid Dolduran Metod
+        {
+            using (MeetingOrganizerEntities db = new MeetingOrganizerEntities())
+            {
+                var sonuc = from t in db.Toplanti
+                            select new
+                            {
+                                t.toplanti_id,
+                                t.toplanti_konusu,
+                                t.tarih,
+                                t.bas_saati,
+                                t.bit_saati,
+                                t.katilimcilar
+                            };
+                dataGridView1.DataSource = sonuc.ToList();
+            }
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (txtKonu.Text != "" && dateTarih.Text != "" && txtBasSaati.Text != "" && txtBitSaati.Text != "" && txtKatilimcilar.Text != "")
+
+            {
+                
+                    using (MeetingOrganizerEntities db = new MeetingOrganizerEntities())
+                    {
+                        Toplanti t = new Toplanti();
+                        t.toplanti_konusu = txtKonu.Text;
+                        t.tarih = dateTarih.Value.ToShortDateString();
+                        t.bas_saati = txtBasSaati.Text;
+                        t.bit_saati = txtBitSaati.Text;
+                        t.katilimcilar = txtKatilimcilar.Text;
+                        db.Toplanti.Add(t);
+                        db.SaveChanges();//veritabanına kaydet
+                        MessageBox.Show("Toplantı Kaydedildi.", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                
+                GridDoldur();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Tüm Kayıtları Doldurun", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            int tdID = 0;
+            if (dataGridView1.SelectedRows.Count > 0)//datagrid seçili ise
+            {
+                tdID = (int)dataGridView1.SelectedRows[0].Cells[0].Value;
+            }
+            using (MeetingOrganizerEntities db = new MeetingOrganizerEntities())
+            {
+                Toplanti t = db.Toplanti.Find(tdID);
+                t.toplanti_konusu = txtKonu.Text;
+                t.tarih = dateTarih.Value.ToShortDateString();
+                t.bas_saati = txtBasSaati.Text;
+                t.bit_saati = txtBitSaati.Text;
+                t.katilimcilar = txtKatilimcilar.Text;
+                db.SaveChanges();
+                MessageBox.Show("Toplantı Güncellendi.", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                foreach (var item in this.Controls)
+                {
+                    if (item is TextBox)
+                    {
+                        (item as TextBox).Clear();
+                    }
+                }
+            }
+            GridDoldur();
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            int tdID = 0;
+
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                tdID = (int)dataGridView1.SelectedRows[0].Cells[0].Value;
+
+                using (MeetingOrganizerEntities db = new MeetingOrganizerEntities())
+                {
+                    Toplanti t = db.Toplanti.Find(tdID);
+                    txtKonu.Text = t.toplanti_konusu;
+                    dateTarih.Text = t.tarih;
+                    txtBasSaati.Text = t.bas_saati;
+                    txtBitSaati.Text = t.bit_saati;
+                    txtKatilimcilar.Text = t.katilimcilar;
+                }
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            DialogResult cevap = MessageBox.Show("Silmek İstediğinize Emin Misiniz?", "UYARI", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (cevap == DialogResult.Yes)
+            {
+
+                int tdID = 0;
+                if (dataGridView1.SelectedRows.Count > 0)
+                {
+                    tdID = (int)dataGridView1.SelectedRows[0].Cells[0].Value;
+                }
+                using (MeetingOrganizerEntities db = new MeetingOrganizerEntities())
+                {
+                    Toplanti t = db.Toplanti.Find(tdID);
+                    db.Toplanti.Remove(t);
+                    db.SaveChanges();
+                    MessageBox.Show("Toplantı Silindi.", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    foreach (var item in this.Controls)
+                    {
+                        if (item is TextBox)
+                        {
+                            (item as TextBox).Clear();
+                        }
+                    }
+                }
+                GridDoldur();
+            }
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            foreach (var item in this.Controls)
+            {
+                if (item is TextBox)
+                {
+                    (item as TextBox).Clear();
+                    
+                }
+                if (item is MaskedTextBox)
+                {
+                    (item as MaskedTextBox).Clear();
+                }
+            }
+            //Tüm Textboxlar Temizlendi
+        }
+
+        private void txtBasSaati_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar); //Toplantı Saati TextBox kısmına sadece Sayı girilmesi sağlandı
+        }
+
+        private void txtBitSaati_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);//Toplantı Saati TextBox kısmına sadece Sayı girilmesi sağlandı
+        }
+
+    }
+}
